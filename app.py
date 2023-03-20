135448622ac72a0e91731068a45f798d62a2db16
u.mitskevich <u.mitskevich@teslasuit.io>
2023-03-21 02:46:38 +0300

    Projects structure made, pdf loader and pdf-to-text added

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import PyPDF2
+import io
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+
+def convert_pdf_to_txt_pages(path):
+    texts = ''
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    size = 0
+    c = 0
+    file_pages = PDFPage.get_pages(path)
+    nbPages = len(list(file_pages))
+    for page in PDFPage.get_pages(path):
+      interpreter.process_page(page)
+      t = retstr.getvalue()
+      if c == 0:
+        texts+=t
+      else:
+        texts+=t
+      c = c+1
+      size = len(t)
+    # text = retstr.getvalue()
+
+    # fp.close()
+    device.close()
+    retstr.close()
+    return texts, nbPages
+
+
+
+pdf_file = st.file_uploader("Load your PDF", type="pdf", on_change=pdf_load)
+
+if pdf_file:
+    path = pdf_file.read()
+    text,_ = convert_pdf_to_txt_pages(pdf_file)
+    st.write(text)
